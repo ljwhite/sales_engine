60453e5089d3da095c6048c48633a8a52ce7e47e
Lito White <leland.white@gmail.com>
2020-08-31 16:53:39 -0600

    JSON API Spec Merchant REST

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'rspec-rails'
 gem 'pry'
 gem 'figaro'
 gem 'simplecov'
+gem 'fast_jsonapi'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
--- spec/requests/api/v1/merchants/merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_request_spec.rb
@@ -6,7 +6,7 @@ describe "Merchant API" do
     get '/api/v1/merchants'
     expect(response).to be_successful
     merchants = JSON.parse(response.body)
-    expect(merchants.count).to eq(3)
+    expect(merchants['data'].count).to eq(3)
   end
 
   it "can get one merchant by its id" do
@@ -14,7 +14,7 @@ describe "Merchant API" do
     get "/api/v1/merchants/#{id}"
     merchant = JSON.parse(response.body)
     expect(response).to be_successful
-    expect(merchant["id"]).to eq(id)
+    expect(merchant['data']['id'].to_i).to eq(id)
   end
 
   it "can create a new merchant" do
